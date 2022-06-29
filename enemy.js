4d39406138e3f6c72cb2ec8ea3be9e5b62062259
Toby-Munyard <67243942+Toby-Munyard@users.noreply.github.com>
2022-06-30 09:13:28 +1200

    Add files via upload
    enemies finally can collide with player

--- enemy.js
+++ enemy.js
@@ -15,22 +15,22 @@ class Enemy extends Player {
         canvasContext.fillRect(this.x, this.y, this.w, this.h);
     }
     EnemyMove() {
-        this.y += this.ySpeed;
+        // this.y += this.ySpeed;
 
-        if (this.y > canvas.height) { // enemy out of bottom of canvas 
-            this.y = 0 - this.w * this.h;
-            this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
-            this.ySpeed = Math.floor(Math.random() * (12 - 4) + 4);
-        }
+        // if (this.y > canvas.height) { // enemy out of bottom of canvas 
+        //     this.y = 0 - this.w * this.h;
+        //     this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
+        //     this.ySpeed = Math.floor(Math.random() * (12 - 4) + 4);
+        // }
 
-        if (this.y + this.w * this.h > super.y && this.y < super.y + super.w * super.h && this.x + this.w * this.h > super.x && this.x < super.x + super.w * super.h) {
-            // player respawns to center of canvas after enemy collides with player
-            this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
-            this.y = canvas.height + 100;
-            super.x = canvas.width / 2;
-            super.y = canvas.height - super.w * super.h * 1.75;
-            console.log("Hello world!");
-        }
+        // if (this.y + this.w * this.h > super.y && this.y < super.y + super.w * super.h && this.x + this.w * this.h > super.x && this.x < super.x + super.w * super.h) {
+        //     // player respawns to center of canvas after enemy collides with player
+        //     this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
+        //     this.y = canvas.height + 100;
+        //     super.x = canvas.width / 2;
+        //     super.y = canvas.height - super.w * super.h * 1.75;
+        //     console.log("Hello world!");
+        // }
 
         // isCollide(this, Player); {
         //     return (
