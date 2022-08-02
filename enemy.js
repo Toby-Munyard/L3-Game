d760ca3bb056c82e4e9d94fc9946ce5bb451e6ac
Toby-Munyard <67243942+Toby-Munyard@users.noreply.github.com>
2022-08-03 10:57:11 +1200

    Add files via upload
    collision between the player and the enemy array is now functioning.

--- enemy.js
+++ enemy.js
@@ -1,6 +1,5 @@
-class Enemy extends Player {
+class Enemy {
     constructor(x, y, h, w, c, xSpeed, ySpeed) {
-        super(x, y, h, w, c, xSpeed, ySpeed);
         this.x = x;
         this.y = y;
         this.w = w;
@@ -26,29 +25,13 @@ class Enemy extends Player {
             this.y = 0 - this.h;
             this.ySpeed = Math.floor(Math.random()*(6 - 3) + 2);
         }
-        // if (this.y > canvas.height) { // enemy out of bottom of canvas 
-        //     this.y = 0 - this.w * this.h;
-        //     this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
-        //     this.ySpeed = Math.floor(Math.random() * (12 - 4) + 4);
-        // }
 
-        // if (this.y + this.w * this.h > super.y && this.y < super.y + super.w * super.h && this.x + this.w * this.h > super.x && this.x < super.x + super.w * super.h) {
-        //     // player respawns to center of canvas after enemy collides with player
-        //     this.x = Math.floor(Math.random() * (canvas.width - this.w * this.h));
-        //     this.y = canvas.height + 100;
-        //     super.x = canvas.width / 2;
-        //     super.y = canvas.height - super.w * super.h * 1.75;
-        //     console.log("Hello world!");
-        // }
-
-        // isCollide(this, Player); {
-        //     return (
-        //         ((this.y + this.h) < (super.y)) ||
-        //         (this.y > (super.y + super.height)) ||
-        //         ((this.x + this.w) < super.x) ||
-        //         (this.x > (super.x + super.width))
-        //     );
-        // };
-
+    }
+    hit(){
+        if(this.x + this.w > player.x && this.x < player.x + 
+            player.w && this.y + this.h > player.y && this.y < player.y + player.h){
+                player.x = 0;
+                health--;
+        }
     }
 }
