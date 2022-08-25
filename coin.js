9ef5bd57974d6c7fabba2b4acc443fa3b677a104
Toby-Munyard <67243942+Toby-Munyard@users.noreply.github.com>
2022-08-26 11:05:31 +1200

    Add files via upload
    Changed all references to 'platform' to now be called 'coin' to make code much more readable and easier to understand.

---
+++ coin.js
@@ -0,0 +1,25 @@
+class Coin {
+    constructor(x, y, w, h, c) {
+         this.x = x;
+         this.y = y;
+         this.w = w;
+         this.h = h;
+         this.c = c;
+
+    }
+    
+    draw() {
+        canvasContext.fillStyle = this.c;
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    hit() {
+        if (this.x + this.w > player.x && this.x < player.x +
+            player.w && this.y + this.h > player.y && this.y < player.y + player.h) {
+            this.y = 0 - this.h;
+            score++
+            console.log("Score: " + score);
+        }
+    }
+
+}
